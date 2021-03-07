e424818c28e718f6057d57bd73d0030c264324b7
Emily Lin <44381949+emilyylin@users.noreply.github.com>
2021-03-07 15:08:46 -0500

    new pages

--- App.js
+++ App.js
@@ -29,7 +29,7 @@
 import * as React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Button, Text, Input } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { default as theme } from './custom-theme.json';
@@ -44,6 +44,22 @@ const styles = StyleSheet.create({
     color: '#FFF',
     textAlign: 'center'
   },
+  button: {
+      height:50,
+      paddingHorizontal:10,
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:10,
+      margin:10,
+  },
+  progressBar: {
+    height: 20,
+    width: '100%',
+    backgroundColor: 'white',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 5
+  }
 });
 
 function Login({ navigation }) {
@@ -57,7 +73,7 @@ function Login({ navigation }) {
   );
 }
 
-function Selection() {
+function Selection({navigation}) {
   const [value, setValue] = React.useState('');
   return (
     <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
@@ -68,6 +84,48 @@ function Selection() {
           value={value}
           onChangeText={nextValue => setValue(nextValue)}
         />
+
+        <Button onPress={() => navigation.navigate('Detecting Emotion')}>Submit Song</Button>
+      </Layout>
+    </ApplicationProvider>
+  );
+}
+
+function playSong() {
+return (
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={styles.text} category='h5'>Playing suggested song</Text>
+      <View style={styles.container}>
+      <Image
+      source={require('./keshi.jpg')}
+      style={{ width: 300, height: 300 }}
+      PlaceholderContent={<ActivityIndicator />}
+      />
+
+    </View>
+    <Text style={styles.text} category='h1'>2 soon</Text>
+    <Image 
+      source={require('./MusicBar.png')}
+      style={{ width: 250, height:  250 }}
+      />
+      
+      </Layout>
+    </ApplicationProvider>
+  );
+}
+
+function detectEmotion({navigation}){
+  return (
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <Button style={{backgroundColor: 'transparent'}} onPress={() => navigation.navigate('Playing Song')}>Loading</Button>
+
+      <Image 
+      source={require('./progressbar.png')}
+      style={{ width: 250, height:  250 }}
+      />
       </Layout>
     </ApplicationProvider>
   );
@@ -81,6 +139,8 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Start Here" component={Login} />
         <Stack.Screen name="Choose a Song" component={Selection} />
+        <Stack.Screen name="Playing Song" component={playSong} />
+        <Stack.Screen name="Detecting Emotion" component={detectEmotion}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
